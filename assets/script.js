7d9660723be4e68ebacd6b1b28050ad7e2205eb5
Alex Marzullo <marzullo94@gmail.com>
2020-12-10 17:48:07 -0500

    Updated JS, 5 day forcast is now loading, and history can now be cleared

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ var searchBtn = document.querySelector("#searchBtn")
 var cityInput = document.querySelector("#cityInput")
 var pastSearch = document.querySelector("#pastSearch")
 
-
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 var apiKey = "a7e2daf70af7df237ffc6f4b3a2f4e20"   
 
@@ -15,10 +16,10 @@ function getWeather(cityName) {
         method: "GET"
       }).then(function(response) {
         console.log(response);
-        document.getElementById("city-name").innerHTML = response.name
-        document.getElementById("temperature").innerHTML = response.main.temp
-        document.getElementById("humidity").innerHTML = response.main.humidity + "%"
-        document.getElementById("wind-speed").innerHTML = response.wind.speed + "mph"
+        document.getElementById("city-name").innerHTML = response.name + "   " + date
+        document.getElementById("temperature").innerHTML = "Temperauture: " + response.main.temp
+        document.getElementById("humidity").innerHTML = "Humidity: " + response.main.humidity + "%"
+        document.getElementById("wind-speed").innerHTML ="Wind speed: " + response.wind.speed + "mph"
         var latCoord = response.coord.lat
         var lonCoord = response.coord.lon
         var uviLink = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latCoord + "&lon=" + lonCoord + "&appid=" + apiKey
@@ -27,7 +28,14 @@ function getWeather(cityName) {
           method: "GET"
         }).then(function(response){
           console.log(response)
-        document.getElementById("UV-index").innerHTML = response.value
+        document.getElementById("UV-index").innerHTML = "UV-index: "+ response.value
+          if(response.value <= 4){
+            document.getElementById("UV-index").setAttribute("style", "color: green")
+          } else if(response.value <= 7){
+            document.getElementById("UV-index").setAttribute("style", "color: yellow")
+          } else{
+            document.getElementById("UV-index").setAttribute("style", "color: red")
+          }
         })
 
       });
@@ -37,28 +45,47 @@ function getWeather(cityName) {
         url: fiveDay,
         method: "GET"
       }).then(function(response){
+        var forecastDiv = document.querySelectorAll(".forecast")
+        var daySelector = -4  
         console.log(response)
+
+        for(var i = 0; i < forecastDiv.length; i++) {
+          var forecastTemp = document.createElement("p")
+          var forecastHum = document.createElement("p")
+          console.log(i)
+          daySelector+= 8;
+          forecastDiv[i].innerHTML = "";
+          forecastTemp.innerHTML = "Temp: " + response.list[daySelector].main.temp + "&#176f"
+          forecastDiv[i].append(forecastTemp)
+
+          forecastHum.innerHTML = "Humidity: " + response.list[daySelector].main.humidity + "%"
+          forecastDiv[i].append(forecastHum)
+          
+
+        }
       })
       
 
 }
-
-
-
 
 searchBtn.addEventListener("click",function() {
     var searchTerm = cityInput.value;
     getWeather(searchTerm);
     searchHistory.push(searchTerm);
     localStorage.setItem("search",JSON.stringify(searchHistory));
-    renderSearchHistory();
+    renderHistory();
+    
 })
 
-function renderSearchHistory() {
+document.getElementById("clearBtn").addEventListener("click",function() {
+  searchHistory = [];
+  renderHistory();
+})
+
+function renderHistory() {
   pastSearch.innerHTML = "";
   for (var i=0; i<searchHistory.length; i++) {
       var historyItem = document.createElement("input");
-      // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
       historyItem.setAttribute("type","text");
       historyItem.setAttribute("readonly",true);
       historyItem.setAttribute("class", "form-control d-block bg-white");
@@ -70,7 +97,7 @@ function renderSearchHistory() {
   }
 }
 
-renderSearchHistory();
+renderHistory();
 if (searchHistory.length > 0) {
   getWeather(searchHistory[searchHistory.length - 1]);
 }
